8879839959f38982baf1e7461ebae4c7534ef9c1
varsha <varsha.vijayakumar07@gmail.com>
2019-11-05 11:55:20 +0530

    Updates

---
+++ precache-manifest.5e505f08b335f8554847432f7cf3b2f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c84e7f90eb0edf43871b8fd44079faa",
+    "url": "/Varsha-Vijayakumar/Country-Details/index.html"
+  },
+  {
+    "revision": "8e1dcb66051be530d02d",
+    "url": "/Varsha-Vijayakumar/Country-Details/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "8e1dcb66051be530d02d",
+    "url": "/Varsha-Vijayakumar/Country-Details/static/js/2.3e1f8dab.chunk.js"
+  },
+  {
+    "revision": "28a1603a34e8ab53aac1",
+    "url": "/Varsha-Vijayakumar/Country-Details/static/js/main.4ed8383c.chunk.js"
+  },
+  {
+    "revision": "7347560de4f736844d96",
+    "url": "/Varsha-Vijayakumar/Country-Details/static/js/runtime-main.21deb151.js"
+  }
+]);
