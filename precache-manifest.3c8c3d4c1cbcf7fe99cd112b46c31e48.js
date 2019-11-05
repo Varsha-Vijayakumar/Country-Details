12a8ec1729cbb677de98a90addb56a1f459fc4c4
Ashik <Ashik@Skitters-MacBook-Pro-4.local>
2019-11-05 15:34:28 +0530

    Updates

--- precache-manifest.3c8c3d4c1cbcf7fe99cd112b46c31e48.js
+++ precache-manifest.3c8c3d4c1cbcf7fe99cd112b46c31e48.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5c84e7f90eb0edf43871b8fd44079faa",
+    "revision": "d5270f1fe12c1c2d6bd8116ecdf98f27",
     "url": "/Varsha-Vijayakumar/Country-Details/index.html"
   },
   {
-    "revision": "8e1dcb66051be530d02d",
+    "revision": "fec03eaa10ab82619bca",
     "url": "/Varsha-Vijayakumar/Country-Details/static/css/2.833dd627.chunk.css"
   },
   {
-    "revision": "8e1dcb66051be530d02d",
+    "revision": "fec03eaa10ab82619bca",
     "url": "/Varsha-Vijayakumar/Country-Details/static/js/2.3e1f8dab.chunk.js"
   },
   {
-    "revision": "28a1603a34e8ab53aac1",
-    "url": "/Varsha-Vijayakumar/Country-Details/static/js/main.4ed8383c.chunk.js"
+    "revision": "554f0a912d3d924519aa",
+    "url": "/Varsha-Vijayakumar/Country-Details/static/js/main.e2769efe.chunk.js"
   },
   {
     "revision": "7347560de4f736844d96",
